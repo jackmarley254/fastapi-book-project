d8b0123a3bed5edbd2a91a1f1cc8793348b24c3c
jackmarley254 <engndiritu1@gmail.com>
2025-02-12 22:39:45 +0300

    Check all requirements

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from routes.books import router as books_router
 from fastapi.middleware.cors import CORSMiddleware
 
 from api.router import api_router
@@ -16,7 +15,6 @@ app.add_middleware(
 )
 
 app.include_router(api_router, prefix=settings.API_PREFIX)
-app.include_router(books_router, prefix="/api/v1/books")
 
 @app.get("/healthcheck")
 async def health_check():
